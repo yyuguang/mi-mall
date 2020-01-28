c94f7bd65305aea74c33a54c3de0bc2752970289
yyuguang <790335255@qq.com>
2020-01-28 17:44:40 +0800

    完成支付宝支付功能

--- src/pages/ali-pay.vue
+++ src/pages/ali-pay.vue
@@ -1,10 +1,47 @@
 <template>
-    
+    <div class="ali-pay">
+        <order-header title="订单支付">
+            <template v-slot:tip>
+                <span>请谨防钓鱼链接或诈骗电话，了解更多</span>
+            </template>
+        </order-header>
+        <loading v-if="loading"></loading>
+        <div class="form" v-html="content"></div>
+    </div>
 </template>
 
 <script>
+    import Loading from "../components/loading";
+    import OrderHeader from "../components/order-header";
+
     export default {
-        name: "ali-pay"
+        name: "ali-pay",
+        components: {OrderHeader, Loading},
+        data() {
+            return {
+                orderId: this.$route.query.orderId,
+                content: '',
+                loading: true
+            }
+        },
+        mounted() {
+            this.paySubmit();
+        },
+        methods: {
+            paySubmit() {
+                this.axios.post('/pay/create', {
+                    orderId: this.orderId,
+                    orderName: 'Vue高仿小米商城',
+                    amount: 0.01,//单位元
+                    payType: 1 //1支付宝，2微信
+                }).then((res) => {
+                    this.content = res.content;
+                    setTimeout(() => {
+                        document.forms[0].submit();
+                    }, 100)
+                })
+            }
+        }
     }
 </script>
 
